ee84df1b026c0b8547a5adf2095c4d383dec5560
Sewon Ahn <phillip@bien.ltd>
2023-06-19 22:07:24 +0900

    배경 모션 작업 중

--- src/components/tree/Background.tsx
+++ src/components/tree/Background.tsx
@@ -1,38 +1,61 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import { Plane, useTexture } from '@react-three/drei'
-import { TextureLoader } from 'three'
 import useTreeStore from '@/stores/useTreeStore'
-
-const bg_200 = require("./assets/backgrounds/200.jpg");
-const bg_300 = require("./assets/backgrounds/300.jpg");
-const bg_400 = require("./assets/backgrounds/400.jpg");
-const bg_500 = require("./assets/backgrounds/500.jpg");
-const bg_600 = require("./assets/backgrounds/600.jpg");
-const bg_700 = require("./assets/backgrounds/700.jpg");
-const bg_800 = require("./assets/backgrounds/800.jpg");
-const bg_obj = require("./assets/backgrounds/bg_objects.png");
+import { useFrame, useThree } from '@react-three/fiber'
 
 const Background = () => {
-  const textures = [
-    new TextureLoader().load(bg_200),
-    new TextureLoader().load(bg_300),
-    new TextureLoader().load(bg_400),
-    new TextureLoader().load(bg_500),
-    new TextureLoader().load(bg_600),
-    new TextureLoader().load(bg_700),
-    new TextureLoader().load(bg_800),
-    new TextureLoader().load(bg_obj),
-  ]
-  const {total} = useTreeStore()
+  const { total } = useTreeStore()
+  const [id, setId] = useState(0)
+  const meshRef1 = useRef()
+  const meshRef2 = useRef()
+  const objRef = useRef()
+  const [fadeIn, setFadeIn] = useState(true)
+  const backgrounds = useTexture([
+    '/assets/backgrounds/000.png',
+    '/assets/backgrounds/200.jpg',
+    '/assets/backgrounds/300.jpg',
+    '/assets/backgrounds/400.jpg',
+    '/assets/backgrounds/500.jpg',
+    '/assets/backgrounds/600.jpg',
+    '/assets/backgrounds/700.jpg',
+    '/assets/backgrounds/800.jpg',
+  ])
+  const object = useTexture('/assets/backgrounds/bg_objects.png')
+  const { width, height } = useThree((state) => state.viewport)
+
+  useFrame(() => {
+    if (meshRef1.current && meshRef2.current) {
+      const speed = 0.01
+      if (fadeIn) {
+        meshRef1.current.material.opacity = Math.min(1, meshRef1.current.material.opacity + speed)
+        meshRef2.current.material.opacity = Math.max(0, meshRef2.current.material.opacity - speed)
+      } else {
+        meshRef1.current.material.opacity = Math.max(0, meshRef1.current.material.opacity - speed)
+        meshRef2.current.material.opacity = Math.min(1, meshRef2.current.material.opacity + speed)
+      }
+    }
+  })
 
   useEffect(() => {
-    console.log(textures)
-  }, [textures])
+    let newId = parseInt((total / 100 - 1).toString())
+    newId = newId < 0 ? 0 : newId
+
+    if (newId !== id) {
+      setId(newId)
+      setFadeIn(!fadeIn)
+    }
+  }, [total])
 
   return (
     <group>
-      <Plane args={[10, 10]} >
-        <meshBasicMaterial attach='material' map={textures[total/100-1]} />
+      <Plane ref={objRef}  args={[width, width*(1086/1255)]} position={[0, height/2-15, -0.9]} >
+        <meshBasicMaterial attach='material' map={object} transparent />
+      </Plane>
+      <Plane ref={fadeIn ? meshRef1 : meshRef2} args={[height, height]} position={[0, 0, -1]}>
+        <meshBasicMaterial attach='material' map={backgrounds[id]} transparent />
+      </Plane>
+      <Plane ref={fadeIn ? meshRef2 : meshRef1} args={[height, height]} position={[0, 0, -1.1]}>
+        <meshBasicMaterial attach='material' map={backgrounds[(id + 1) % backgrounds.length]} transparent />
       </Plane>
     </group>
   )
