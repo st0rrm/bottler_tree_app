1fd98aeb208907c3b9021b0ec8a266f6fc63bd35
Sewon Ahn <phillip@bien.ltd>
2023-07-04 19:12:38 +0900

    Add save and load interface

--- app/tree/page.tsx
+++ app/tree/page.tsx
@@ -2,7 +2,7 @@
 'use client'
 
 import dynamic from 'next/dynamic'
-import React, { useEffect, useRef, useState, lazy, Suspense } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import Tree from '@/components/tree/Tree'
 import { LSystemInstanced } from '@/components/tree/LSystemInstanced'
 import Cover from '@/components/tree/Cover'
@@ -13,16 +13,15 @@ import Score from '@/components/tree/Score'
 import { SendToMobile, useMobileStatus } from '@/helpers/SendToMobile'
 import { db } from '@/components/tree/db'
 
-
 const View = dynamic(() => import('@/components/canvas/View').then((mod) => mod.View), { ssr: false })
 const Common = dynamic(() => import('@/components/canvas/View').then((mod) => mod.Common), { ssr: false })
 
 type Data = {
-  type: string,
-  uid: string,
-  score: number,
-  total: number,
-  count: number,
+  type: string
+  uid: string
+  score: number
+  total: number
+  count: number
 }
 
 export default function Page() {
@@ -30,11 +29,11 @@ export default function Page() {
   const scoreRef = useRef()
   const treeRef = useRef()
   const [treeView, setTreeView] = useState(null) // useState 추가
-  const isMobile = useMobileStatus();
+  const isMobile = useMobileStatus()
 
   useEffect(() => {
-    document.addEventListener("message", receiveMessage);
-    window.addEventListener("message", receiveMessage);
+    document.addEventListener('message', receiveMessage)
+    window.addEventListener('message', receiveMessage)
 
     setTreeView(
       <View debug={false} className='absolute top-0 flex h-screen w-full flex-col items-center justify-center' orbit>
@@ -43,27 +42,26 @@ export default function Page() {
         </Tree>
         <Background />
         <Common color={'#EFEFED'} />
-      </View>
+      </View>,
     )
 
     return () => {
-      document.removeEventListener("message", receiveMessage);
-      window.removeEventListener("message", receiveMessage);
+      document.removeEventListener('message', receiveMessage)
+      window.removeEventListener('message', receiveMessage)
     }
   }, [])
 
   useEffect(() => {
-    if(treeRef.current){
-      SendToMobile("COMMAND", "hello");
+    if (treeRef.current) {
+      SendToMobile('COMMAND', 'hello')
     }
   }, [treeRef.current])
 
   const init = async (uid, total, count) => {
-
     // alert("03. ------- ", db)
-    const saveTotal = await db.option.where({ uid:uid, key: "total" }).first()
-    const saveCount = await db.option.where({ uid:uid, key: "count" }).first()
-    if(saveTotal && saveCount) {
+    const saveTotal = await db.option.where({ uid: uid, key: 'total' }).first()
+    const saveCount = await db.option.where({ uid: uid, key: 'count' }).first()
+    if (saveTotal && saveCount) {
       // load data from indexedDB
       treeRef.current.load(uid)
     } else {
@@ -76,7 +74,7 @@ export default function Page() {
     setUid(uid)
     setInitThree(true)
     if (isMobile) {
-      SendToMobile("COMMAND", "init");
+      SendToMobile('COMMAND', 'init')
     }
   }
 
@@ -85,14 +83,18 @@ export default function Page() {
     treeRef.current.generating(total, count, 1)
   }
 
-  const receiveMessage = (e:any)=>{
-    try{
+  const receiveMessage = (e: any) => {
+    try {
       const data = JSON.parse(e.data)
       const { type, uid, total, count, score } = data
-      if(type === 'grow'){
-        if(treeRef.current && score && total && count) grow(score, total, count)
-      }else if(type === 'init'){
-        if(treeRef.current && uid && total && count) init(uid, total, count)
+      if (type === 'grow') {
+        if (treeRef.current && score && total > -1 && count) grow(score, total, count)
+      } else if (type === 'init') {
+        if (treeRef.current && uid && total > -1 && count) init(uid, total, count)
+      } else if (type === 'save') {
+        if (treeRef.current && uid) treeRef.current.save(uid)
+      } else if (type === 'load') {
+        if (treeRef.current && uid) treeRef.current.load(uid)
       }
     } catch (e) {
       //
@@ -125,21 +127,27 @@ export default function Page() {
   }
 
   const Buttons = () => {
-    <>
+    ;<>
       <div className={'fixed w-24 h-16 bottom-40 right-0 bg-amber-300 z-40'}>
-        <button onClick={() => handleGrow({ uid: "a", score: 5, total: total+5, count: count+1 })} className={'w-full h-full'}>
+        <button
+          onClick={() => handleGrow({ uid: 'a', score: 5, total: total + 5, count: count + 1 })}
+          className={'w-full h-full'}
+        >
           GROW + 5
         </button>
       </div>
 
       <div className={'fixed w-24 h-16 bottom-16 right-0 bg-amber-300 z-40'}>
-        <button onClick={() => handleInit({ uid: "bbbb", total: 0, count: 1, score: 0 })} className={'w-full h-full'}>
+        <button onClick={() => handleInit({ uid: 'bbbb', total: 0, count: 1, score: 0 })} className={'w-full h-full'}>
           INIT + 0
         </button>
       </div>
 
       <div className={'fixed w-24 h-16 bottom-16 left-0 bg-amber-300 z-40'}>
-        <button onClick={() => handleInit({ uid: "bbbb", total: 800, count: 100, score: 0 })} className={'w-full h-full'}>
+        <button
+          onClick={() => handleInit({ uid: 'bbbb', total: 800, count: 100, score: 0 })}
+          className={'w-full h-full'}
+        >
           INIT + 800
         </button>
       </div>
